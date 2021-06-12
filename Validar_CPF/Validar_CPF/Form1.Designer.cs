b6c23017db2400ad1dcfae9c6c06a8a62fa8b7f2
man7698 <matheusalmeida7698@gmail.com>
2021-06-12 00:15:36 -0300

    Add files via upload

---
+++ Validar_CPF/Validar_CPF/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Validar_CPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtvalor = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o CPF:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(328, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(139, 36);
+            this.txtvalor.Mask = "000,000,000-00";
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(169, 20);
+            this.txtvalor.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 136);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox txtvalor;
+    }
+}
+
---
+++ Validar_CPF/Validar_CPF/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Validar_CPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };// primeiro vetor para 1º multiplicação
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };//segundo vetor para 2º multiplicação
+            string CPF = txtvalor.Text;// recebe o cpf
+            string auxCPF;
+            string Digito;
+            int soma;
+            int resto;
+
+
+            CPF = CPF.Trim();//remove todos espaçoes em branco se tiver, tanto no começo como no final
+            CPF = CPF.Replace(".", "").Replace("-", "");//remove todos pontos ou traços e substitui por nada no lugar
+
+            auxCPF = CPF.Substring(0, 9);//aux do cpf pega todos 9 primeiros digitos do valor digitado
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                //vai multiplicando e somando ao mesmo tempo para economizar linha com vetor multiplicador1
+                soma += int.Parse(auxCPF[i].ToString()) * multiplicador1[i];
+            }
+
+            resto = soma % 11;//descobre o resto de divisão
+
+            if (resto < 2)
+            {
+                resto = 0;// se menor que 2 vai valer 0
+            }
+            else
+            {
+                resto = 11 - resto; // senao é 11 menos o resto
+            }
+
+            Digito = resto.ToString(); // guarda o primeiro digito
+            auxCPF = auxCPF + Digito; // passa pra aux do cpf
+
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)//conta 10 porque tem um digito a mais agora
+            {
+                //vai multiplicando e somando ao mesmo tempo pelo vetor multiplicador2 que é a segunda tabelinha
+                soma += int.Parse(auxCPF[i].ToString()) * multiplicador2[i];
+            }
+
+            resto = soma % 11;//pega o resto da divisao
+
+
+            if (resto < 2)
+            {
+                resto = 0;
+            }
+            else
+            {
+                resto = 11 - resto;
+            }
+
+
+            auxCPF = auxCPF + resto;// passa o ultimo digito restante pra comparar
+
+
+            if (CPF == auxCPF)//se o digitado no campo de texto for igual ao que foi processado aqui é porque é valido
+            {
+                MessageBox.Show("O CPF é Valido !!");
+            }
+            else
+            {
+                MessageBox.Show("O CPF não é Valido !!");
+            }
+
+
+        }
+    }
+}
